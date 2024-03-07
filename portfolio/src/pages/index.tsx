3680803c54efa6e60bd6d7fe6e3e473dcf6a6154
Samuel Wood <samote.wood@gmail.com>
2024-03-06 18:08:56 -0800

    fixing script head tag

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,23 +1,24 @@
 import Head from 'next/head'
+import Script from 'next/script';
 import HomePageComponent from '@/components/HomePage'
 import clsx from 'clsx'
 
 export default function Home() {
   return (
     <>
+      <Script strategy="beforeInteractive">
+        {`
+            document.documentElement.classList.add('dark');
+        `}
+      </Script>
+
       <Head>
         <title>Sam Wood - FullStack Engineer</title>
         <meta name="description" content="Hi I'm Sam! I build things!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                document.documentElement.classList.add('dark');
-            `,
-          }}
-        />
       </Head>
+
       <main
         className={clsx(
           'min-h-[100vh] px-4 py-8 md:py-[5rem] flex justify-center flex-col items-center',
